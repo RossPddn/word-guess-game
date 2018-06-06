8885cf8563767597778344f41584a76ffc9d190a
Ross <paddon.ross@gmail.com>
2018-06-06 14:31:04 -0700

    new stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,29 @@
 var wins = 0;
 var guesRemaining = 13; 
 wrongGuesses=[];
+answerArray=[];
 document.getElementById('guessesRemaining').innerText = guesRemaining;
-
-var words = ['Mars', 'Venus', 'Jupiter', 'Uranus', 'Saturn']
+var answer;
+var words = ['mars', 'venus', 'jupiter', 'uranus', 'saturn']
 var randomword = words[Math.round(Math.random()*4)];
 console.log(randomword);
+
+function word(){
+    for(var i = 0; i<randomword.length;i++){
+        answerArray[i]="_";
+
+    }
+    answer = answerArray.join("");
+document.getElementById("answer").innerHTML = answer;
+}
 
 
 function checkArray(event){
     var key = event.key;
     //if the chosen letter is in the random word do this, else do that
    if(randomword.includes(key)){
-       console.log(true);
+       
+       
 
 
    }else{
@@ -23,8 +34,9 @@ function checkArray(event){
     
 }
 
+
 }
 
 //RESET guesses and word
-
+word();
 document.onkeyup = checkArray;

2a3b7cdd42c6c88f25ae69db8a6b0a14e7c8d6d5
Ross <paddon.ross@gmail.com>
2018-06-06 12:26:19 -0700

    a bit of javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,30 @@
 var wins = 0;
 var guesRemaining = 13; 
-
+wrongGuesses=[];
 document.getElementById('guessesRemaining').innerText = guesRemaining;
+
+var words = ['Mars', 'Venus', 'Jupiter', 'Uranus', 'Saturn']
+var randomword = words[Math.round(Math.random()*4)];
+console.log(randomword);
+
+
+function checkArray(event){
+    var key = event.key;
+    //if the chosen letter is in the random word do this, else do that
+   if(randomword.includes(key)){
+       console.log(true);
+
+
+   }else{
+       wrongGuesses.push(key);
+       guesRemaining--;
+       document.getElementById('wrong').innerText = wrongGuesses;
+       document.getElementById('guessesRemaining').innerText = guesRemaining;
+    
+}
+
+}
+
+//RESET guesses and word
+
+document.onkeyup = checkArray;
